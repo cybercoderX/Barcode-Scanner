d5c513dcce99fd43490be36a54666558c2643097
cybercoderX <73379395+cybercoderX@users.noreply.github.com>
2021-06-13 11:58:39 +0530

    Add files via upload

---
+++ barcodescanner/barcodescanner/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './screens/BookTransactionScreen';
+import SearchScreen from './screens/SearchScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+},{
+
+defaultNavigationOptions : ({navigation}) =>(
+
+{
+
+tabBarIcon : () => {
+ const route = navigation.state.routeName ;
+    
+    if ( route  == "Transaction"){
+
+      return 
+        
+    }
+}
+
+
+}
+
+
+)
+
+
+
+
+
+});
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
